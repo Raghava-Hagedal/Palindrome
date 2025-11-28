08b91b2e98e17fb2b80eeec5a94580b02e2a9223
Raghava-Hagedal <raghavahagedal@gmail.com>
2025-11-28 22:44:28 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,10 +1,14 @@
 import sys
-if len(sy_avg)==2:
-script_name=sys.avg[0]
-name=sys.avg[1]
+
+# Check if exactly 2 arguments are passed (script name + 1 value)
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
 else:
-name="Gadag"
-if name==name[::-1]:
-print(f"{name} is a palindrome")
+    name = "Gadag"
+
+# Check palindrome
+if name == name[::-1]:
+    print(f"{name} is a palindrome")
 else:
-print(f"{name} is not a palindrome")
+    print(f"{name} is not a palindrome")
