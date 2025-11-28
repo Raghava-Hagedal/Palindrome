573e7c7d72f27223ddc5745f7609aa39f5b64c56
Raghava-Hagedal <raghavahagedal@gmail.com>
2025-11-28 22:36:20 +0530

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,10 @@
+import sys
+if len(sy_avg)==2:
+script_name=sys.avg[0]
+name=sys.avg[1]
+else:
+name="Gadag"
+if name==name[::-1]:
+print(f"{name} is a palindrome")
+else:
+print(f"{name} is not a palindrome")
